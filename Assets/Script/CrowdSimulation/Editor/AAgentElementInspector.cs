48cf84736e7defc13e8a5b7a4f5ff9253cda8221
JJ <a283292040@qq.com>
2019-01-16 18:12:57 +0800

    Add Recorder and CineMachine.

---
+++ Assets/Script/CrowdSimulation/Editor/AAgentElementInspector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AAgentElement))]
+public class AAgentElementInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AAgentElement currentTarget = target as AAgentElement;
+
+        GUI.skin.label.fontSize = 16;
+        GUILayout.Label("Monitor");
+
+        GUI.skin.label.fontSize = 12;
+        EditorGUILayout.LabelField("Current state", currentTarget.currentAgentState.ToString());
+        EditorGUILayout.LabelField("Current destination", currentTarget.agentNavDestination.ToString());
+
+    }
+}
